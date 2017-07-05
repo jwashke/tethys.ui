69038d902c24ca48e52fcd7ec3c176384d24f508
Josh Washke <jwashke@icloud.com>
2017-07-04 22:19:17 -0600

    Change signin form to model driven form

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../models/';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  user: User;
 
   constructor() { }
 
   ngOnInit() {
+      this.user = new User();
+  }
+
+  onSubmit(): void {
+    console.log(this.user);
   }
 
 }
