41995671209cec33df6ccbe073c0cf7b744f6816
Josh Washke <jwashke@icloud.com>
2017-07-04 21:32:22 -0600

    Create navbar, create signin component and routes for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'signin', component: SigninComponent },
+  { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
