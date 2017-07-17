2648451de0ce5855b436b98fcea5dcef3222d9a9
Josh Washke <jwashke@icloud.com>
2017-07-17 16:33:53 -0600

    Add Sign out button to navbar, add signedIn Observable to auth service

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,4 +7,11 @@ import { AuthService } from '../shared/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  signedIn: any;
+
+  constructor(private _authService: AuthService) {
+    this._authService.isSignedIn
+      .subscribe(signedIn => this.signedIn = signedIn);
+  }
+}
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,28 +1,33 @@
 import { Injectable } from '@angular/core';
 import { LocalStorageService } from 'angular-2-local-storage';
 
+import { Subject } from 'rxjs/Subject';
+
 @Injectable()
 export class AuthService {
-  private auth_token: any;
+  private authToken: any;
+  private signedIn: Subject<boolean> = new Subject<boolean>();
 
-  constructor (
-    private _localStorageService: LocalStorageService
-  ) { }
+  get isSignedIn() {
+      return this.signedIn.asObservable();
+  }
+
+  constructor (private _localStorageService: LocalStorageService) {
+    this.signedIn.next(this.getTokenFromStorage() ? true : false);
+  }
 
   setToken(token: string): void {
     this._localStorageService.set('auth_token', token);
-    this.auth_token = token;
+    this.authToken = token;
+    this.signedIn.next(true);
   }
 
   getToken(): string {
-    return this.auth_token ? this.auth_token : this.getTokenFromStorage();
+    return this.authToken ? this.authToken : this.getTokenFromStorage();
   }
 
   getTokenFromStorage(): string {
-    if (this._localStorageService.get('auth_token')) {
-      this.auth_token = this._localStorageService.get('auth_token');
-      return this.auth_token
-    } else
-      return undefined;
+    this.authToken = this._localStorageService.get('auth_token');
+    return this.authToken;
   }
 }
